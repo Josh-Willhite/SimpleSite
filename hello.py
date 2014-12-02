d7dfca8eb3649ebcde27e1dbb316e1a8070054bc
Josh Willhite <jwillhite@gmail.com>
2014-12-02 13:45:12 -0800

    use env variable to set port, in order to test deploying to different environments

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
+import os
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
-app.config.from_envvar('FLASK_SETTINGS')
 
 @app.route('/hello/<name>')
 def hello_Janrain(name=None):
     return render_template('index.html', name=name) 
 
 if __name__ == "__main__":
-    app.run()
+    try:
+        port = os.environ['FLASK_PORT']
+    except Exception as err:
+        port = "5000"
+    
+    print "Serving on port: %s" % port
+
+    app.run(port=int(port))
