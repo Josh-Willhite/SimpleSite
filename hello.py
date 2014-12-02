ec37993723ac53149dd62c24aedf9bd00985cdfe
Josh Willhite <jwillhite@gmail.com>
2014-12-02 13:36:31 -0800

    convereted back to use default port

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+app.config.from_envvar('FLASK_SETTINGS')
 
 @app.route('/hello/<name>')
 def hello_Janrain(name=None):
     return render_template('index.html', name=name) 
 
 if __name__ == "__main__":
-    app.run(port='80')
+    app.run()
